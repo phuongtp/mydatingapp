e4e83766fa97dde0a429aa2aae7be9f0a6c9aff5
PhuongPhan <phuongtp@gmail.com>
2021-05-06 15:25:03 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,8 @@ namespace API
         options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
       });
       services.AddControllers();
+
+      services.AddCors();
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -55,6 +57,7 @@ namespace API
 
       app.UseRouting();
 
+      app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints =>
