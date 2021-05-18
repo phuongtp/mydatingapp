782cd9b1378500d86b50a511749d1e9e52da78ac
PhuongPhan <phuongtp@gmail.com>
2021-05-18 13:53:32 +0200

    End of Section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -16,8 +17,10 @@ namespace API.Controllers
   public class UsersController : BaseApiController
   {
     private readonly IUserRepository _userRepository;
-    public UsersController(IUserRepository userRepository)
+    private readonly IMapper _mapper;
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
+      _mapper = mapper;
       _userRepository = userRepository;
     }
 
@@ -37,6 +40,21 @@ namespace API.Controllers
       var user = await _userRepository.GetMemberByUsernameAsync(username);
       // var userToReturn = _mapper.Map<MemberDto>(user);
       return Ok(user);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+      var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var user = await _userRepository.GetUserByUsernameAsync(username);
+
+      _mapper.Map(memberUpdateDto, user);
+
+      _userRepository.Update(user);
+
+      if (await _userRepository.SaveAllAsync()) return NoContent();
+
+      return BadRequest("Failed to update user");
     }
   }
 }
