7087d7be16e9a34b0372736cd38d3e1e4ede7c74
PhuongPhan <phuongtp@gmail.com>
2021-05-18 17:10:28 +0200

    End of Section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -30,8 +30,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
         return user;
       })
