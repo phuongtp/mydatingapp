11dc8e33490a003767924bb447f288e101829e0a
PhuongPhan <phuongtp@gmail.com>
2021-05-20 17:25:06 +0200

    End of Section 15.2

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -72,5 +72,26 @@ namespace API.Controllers
       return Ok(await _unitOfWork.MessageRepository.GetMessageThread(currentUsername, username));
 
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteMessage(int id)
+    {
+      var username = User.GetUsername();
+
+      var message = await _unitOfWork.MessageRepository.GetMessage(id);
+
+      if (message.Sender.UserName != username && message.Recipient.UserName != username)
+        return Unauthorized();
+
+      if (message.Sender.UserName == username) message.SenderDeleted = true;
+      if (message.Recipient.UserName == username) message.RecipientDeleted = true;
+
+      if (message.SenderDeleted && message.RecipientDeleted)
+        _unitOfWork.MessageRepository.DeleteMessage(message);
+
+      if (await _unitOfWork.Complete()) return Ok();
+
+      return BadRequest("Problem deleting the message");
+    }
   }
 }
